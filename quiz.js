7d5c00c216c3ce65f27ce83f8b0e43344717f1ac
Divyansh Rai <divyansh16rai@gmail.com>
2025-08-01 09:17:15 +0530

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -111,7 +111,7 @@ let globalTimeInterval;
 function startGlobalTimer() {
     globalTimeInterval = setInterval (() => {
         totalTime--;
-        document.getElementById("global-timer").textContent = totalTime;
+        document.getElementById("timer").textContent = totalTime;
         if (totalTime <= 0) {
             clearInterval(globalTimeInterval);
             alert("Time's up for quiz!");
